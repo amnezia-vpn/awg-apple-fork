4e25561168d76c50a970aa8b42046bd587b84f3f
Macbook <macbook@MacBook-Pro.local>
2024-11-28 21:40:03 +0700

    update

--- Sources/WireGuardKitC/WireGuardKitC.h
+++ Sources/WireGuardKitC/WireGuardKitC.h
@@ -1,11 +1,8 @@
 // // SPDX-License-Identifier: MIT
 // // Copyright © 2018-2023 WireGuard LLC. All Rights Reserved.
 
-
 #include "key.h"
 #include "x25519.h"
-
-
 
 #include "key.h"
 #include "x25519.h"
